277a249136e9681fb0cad4f89a38e1650ad6d8bf
Brianna Saglimbeni <briannabeni@Briannas-MacBook-Air.local>
2024-02-24 14:22:22 -0500

    added the code to initialize the react router

--- src/main.jsx
+++ src/main.jsx
@@ -12,8 +12,8 @@ import Contact from './pages/Contact.jsx'
 import Error from './pages/Error.jsx'
 import './index.css'
 
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+<RouterProvider router={router} />
+);
