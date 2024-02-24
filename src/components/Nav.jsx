161ae13fffbff56ee638d80a3e02a80a72da6eef
Brianna Saglimbeni <briannabeni@Briannas-MacBook-Air.local>
2024-02-24 14:13:49 -0500

    added comments to the Nav.jsx page

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -8,21 +8,26 @@ function Nav(){
             <li className="nav-item">
                 <Link
                    to="/"
+          // This is a conditional (ternary) operator that checks to see if the current page is "Home"
+          // If it is, we set the current page to 'nav-link-active', otherwise we set it to 'nav-link'
                    className={currentPage === "/" ? "nav-link active" : "nav-link"}>Home</Link>
             </li>
             <li className="nav-item">
                 <Link
                     to="/About"
+          // Check to see if the currentPage is `About`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
                     className={currentPage === "/About" ? "nav-link active" : "nav-link"}>About</Link>
             </li>
             <li className="nav-item">
                 <Link
                   to="/Projects"
+            // Check to see if the currentPage is `Projects`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
                   className={currentPage === "/Projects" ? "nav-link active" : "nav-link"}>Projects</Link>
             </li>
             <li className="nav-item">
                 <Link
                   to="/Contact"
+            // Check to see if the currentPage is `Contact`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
                   className={currentPage === "/Contact" ? "nav-link active" : "nav-link"}>Contact</Link>
             </li>
         </ul>
