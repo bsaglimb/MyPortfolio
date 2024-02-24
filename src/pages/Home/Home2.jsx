1483d8693bdd900b382eb65e65e09e6310e01e07
Brianna Saglimbeni <briannabeni@Briannas-MacBook-Air.local>
2024-02-24 15:53:26 -0500

    made a home page 2 file and added imports

---
+++ src/pages/Home/Home2.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import Tilt from "react-parallax-tilt";
+import {
+  AiFillGithub,
+  AiOutlineTwitter,
+  AiFillInstagram,
+} from "react-icons/ai";
+import { FaLinkedinIn } from "react-icons/fa";
