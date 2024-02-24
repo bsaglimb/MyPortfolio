bad6aca562097c27258c19ccc4dc1af2e046c3b3
Brianna Saglimbeni <briannabeni@Briannas-MacBook-Air.local>
2024-02-24 14:18:52 -0500

    imported files into the main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+// Bringing in the required imports from 'react-router-dom' to set up application routing behavior
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css'
+
 import App from './App.jsx'
+import Home from './pages/Home.jsx'
+import About from './pages/About.jsx'
+import Projects from './pages/Projects.jsx'
+import Contact from './pages/Contact.jsx'
+import Error from './pages/Error.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
