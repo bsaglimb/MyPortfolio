a64a935c169e7f97a9339b9af00b8a2e3c6cbfe1
Brianna Saglimbeni <briannabeni@Briannas-MacBook-Air.local>
2024-02-24 16:23:45 -0500

    added github and linkedin links to Home2.jsx

--- src/pages/Home/Home2.jsx
+++ src/pages/Home/Home2.jsx
@@ -1,10 +1,7 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import Tilt from "react-parallax-tilt";
 import {
   AiFillGithub,
-  AiOutlineTwitter,
-  AiFillInstagram,
 } from "react-icons/ai";
 import { FaLinkedinIn } from "react-icons/fa";
 
@@ -42,6 +39,29 @@ export default function Home2() {
               </i>
                     </p>
                 </Col>
+                <Col md={12} className="home-about-social">
+            <h1>Feel free to connect with me</h1>
+            <ul className="home-about-social-links">
+              <li className="social-icons">
+                <a
+                  href="https://github.com/bsaglimb"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-color  home-social-icons">
+                  <AiFillGithub />
+                </a>
+                </li>
+                <li className="social-icons">
+                <a
+                  href="https://www.linkedin.com/in/brianna-s-37ab3b14a/"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-color  home-social-icons">
+                  <FaLinkedinIn />
+                </a>
+                </li>
+            </ul>
+            </Col>
             </Row>
         </Container>
         </Container>
