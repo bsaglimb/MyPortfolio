654d0e52aea6b05dece40e71377a7961807e1f91
Brianna Saglimbeni <briannabeni@Briannas-MacBook-Air.local>
2024-02-24 15:26:25 -0500

    added particles-bg and started to edit the homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,23 @@
+import React from "react";
+import ParticlesBg from "particles-bg";
+import { Container, Row, Col } from "react-bootstrap";
+
 export default function Home() {
     return (
-      <div>
-        <h1>Home Page</h1>
-      </div>
+      <section>
+        <Container fluid className="home-section" id="home">
+        <ParticlesBg type="lines" bg={true} />
+          <Container className="home-content">
+            <Row>
+              <Col md={7} className="home-header">
+                <h1 style={{ paddingBottom: 15 }} className="heading">Hello, welcome to my portfolio!</h1>
+                <h1 className="heading-name">I'm
+                  <strong className="main-name"> Brianna Saglimbeni</strong>
+                </h1>
+              </Col>
+            </Row>
+          </Container>
+        </Container>
+      </section>
     );
   }
