818b9b949613d427bccbea3bdf1d6c9ea87a2cbc
Brianna Saglimbeni <briannabeni@Briannas-MacBook-Air.local>
2024-02-24 14:11:42 -0500

    added link to the home, about, project, and contact pages in NAv.jsx file

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,6 +4,28 @@ function Nav(){
     const currentPage = useLocation().pathname;
 
     return(
+        <ul className="nav nav-bar">
+            <li className="nav-item">
+                <Link
+                   to="/"
+                   className={currentPage === "/" ? "nav-link active" : "nav-link"}>Home</Link>
+            </li>
+            <li className="nav-item">
+                <Link
+                    to="/About"
+                    className={currentPage === "/About" ? "nav-link active" : "nav-link"}>About</Link>
+            </li>
+            <li className="nav-item">
+                <Link
+                  to="/Projects"
+                  className={currentPage === "/Projects" ? "nav-link active" : "nav-link"}>Projects</Link>
+            </li>
+            <li className="nav-item">
+                <Link
+                  to="/Contact"
+                  className={currentPage === "/Contact" ? "nav-link active" : "nav-link"}>Contact</Link>
+            </li>
+        </ul>
 
     );
 }
