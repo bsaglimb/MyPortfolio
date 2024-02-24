5e0a84724b44c8c9c5e326d94bfa2c885f061aae
Brianna Saglimbeni <briannabeni@Briannas-MacBook-Air.local>
2024-02-24 14:53:17 -0500

    imported necessary files to the App.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,31 @@
-import { useState,  } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Header from './components/Header'
+import { useState } from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Home from "./assets/pages/Home";
+import About from "./assets/pages/About";
+import Projects from "./pages/Projects";
+import Contact from "./pages/Contact";
+import Error from "./pages/Error";
+import Nav from "./components/Nav";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-    <Header />
+      <Nav />
+      <Header />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
