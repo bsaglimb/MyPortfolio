a1c2f6e881b9d5f14a7553e93f76876442ab3be2
Brianna Saglimbeni <briannabeni@Briannas-MacBook-Air.local>
2024-02-25 00:06:55 -0500

    added Particles.jsx file and code

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,25 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import Particle from "../components/Particles";
+
+
 export default function About() {
     return (
-      <div>
-        <h1>About Page</h1>
-      </div>
+      <Container fluid className="about-section">
+      <Particle />
+      <Container>
+        <Row style={{ justifyContent: "center", padding: "10px" }}>
+          <Col
+            md={7}
+            style={{
+              justifyContent: "center",
+              paddingTop: "30px",
+              paddingBottom: "50px",
+            }}
+          >
+      </Col>
+      </Row>
+      </Container>
+      </Container>
     );
   }
