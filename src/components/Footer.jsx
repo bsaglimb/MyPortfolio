e52d8799894e9f982f395c6c1b5154f24aef6d33
Brianna Saglimbeni <briannabeni@Briannas-MacBook-Air.local>
2024-02-25 01:05:04 -0500

    added imports and my copyright to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,21 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import {AiFillGithub,} from "react-icons/ai";
+import { FaLinkedinIn } from "react-icons/fa";
+
 export default function Footer() {
+  let date = new Date();
+  let year = date.getFullYear();
     return (
-      <div>
-        <h1>Footer</h1>
-      </div>
+      <Container fluid className="footer">
+      <Row>
+        <Col md="4" className="footer-copywright">
+          <h3>Designed and Developed by Brianna Saglimbeni</h3>
+        </Col>
+        <Col md="4" className="footer-copywright">
+          <h3>Copyright © {year} BS</h3>
+        </Col>
+        </Row>
+        </Container>
     );
   }
