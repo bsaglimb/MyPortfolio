813e933821615cd1ffe2109e5e9a696173ed4c34
Brianna Saglimbeni <briannabeni@Briannas-MacBook-Air.local>
2024-02-27 14:05:19 -0500

    imported more pdf files to Resume.jsx

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -2,7 +2,9 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import { Document, Page, pdfjs } from "react-pdf";
-
+import "react-pdf/dist/esm/Page/AnnotationLayer.css";
+import pdf from "../assets/Resume.pdf";
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 
 export default function Resume() {
@@ -13,6 +15,8 @@ export default function Resume() {
     }, []);
 
     return (
+
+        
     );
 
 }
